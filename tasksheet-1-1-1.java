ccd3019ff5199fac4f6bb4e311a77e01429404ee
KennethCarlos404 <kennethcarlos.sti@gmail.com>
2024-11-25 07:09:08 +0000

    commit

--- tasksheet-1-1-1.java
+++ tasksheet-1-1-1.java
@@ -4,6 +4,6 @@
  */
 class helloWorldApp {
     public static void main(String[] args) {
-        System.out.rintln("Hello World!"); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
