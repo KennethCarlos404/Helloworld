71ee325f2aed5db8acb7b0ed815dfc215807569d
KennethCarlos404 <kennethcarlos.sti@gmail.com>
2024-11-25 06:49:09 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
 
+
+public class Main{
+   static public void main(String... args) {
+    int i=10;
+    int n = --i%5;
+    System.out.println("how ");
+    System.out.println("this is i "+ i );
+    System.out.println("this is n " + n);
+    System.out.println(i);
+
+    /** for( i=1; i<=10 ; i++){
+        for(i=1; i<=5 ; i++){
+            System.out.println("Hi There");
+        }
+
+    }
+    
+    while (true){
+        System.out.println("Waine");
+    }
+    */
+}
+}
